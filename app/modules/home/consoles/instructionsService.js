9c118d6c6e0d28ed50f4d1e6037238b117113819
Joey Gauthier <digitaljedi@outlook.com>
2016-05-30 12:30:54 -0500

    fixed variable reference bug

--- app/modules/home/consoles/instructionsService.js
+++ app/modules/home/consoles/instructionsService.js
@@ -23,7 +23,10 @@
 			remFive: {ins: 'Press the "5" button on the top half of the remote to the right of the "Input" button.', image: false},
 			commentDvr: {ins: 'If you look at the surround receiver you should now see "DVR/VCR".', image: false},
 			tvNavAv: {ins: 'Using the left and right arrow keys on the remote, navigate to "AV1" and press the "OK" button.', image: false},
-			eightPortSwitch: {ins: 'On the 8-port switch, press the purple button under IN ', image: false}
+			tvNavComp1: {ins: 'Using the left and right arrow keys on the remote, navigate to "Composite1" and press the "OK" button.', image: false},
+			eightPortSwitch: {ins: 'On the 8-port switch, press the purple button under "IN ', image: false},
+			powerOn: {ins: 'Press the power button on the console.', image: false},
+			finishedOn: {ins: 'Your done. Enjoy!', image: false}
 		};
 
 
@@ -34,21 +37,34 @@
 
 		function getInstructionArray (con) {
 			var consoleArrs = {
-				genesis: {arr: ['remTvMedia', 'tvNavAv', 'remAvrGame', 'remFive', 'commentDvr', 'eightPortSwitch'], params: {portNumber: '1'}}
+				genesis: {arr: ['remTvMedia', 'tvNavAv', 'remAvrGame', 'remFive', 'commentDvr', 'eightPortSwitch', 'powerOn', 'finishedOn'],
+					params: {portNumber: '1'}},
+				gamecube: {arr: ['remTvMedia', 'tvNavAv', 'remAvrGame', 'remFive', 'commentDvr', 'eightPortSwitch', 'powerOn', 'finishedOn'],
+					params: {portNumber: '2'}},
+				nes: {arr: ['remTvMedia', 'tvNavAv', 'remAvrGame', 'remFive', 'commentDvr', 'eightPortSwitch', 'powerOn', 'finishedOn'],
+					params: {portNumber: '3'}},
+				n64: {arr: ['remTvMedia', 'tvNavAv', 'remAvrGame', 'remFive', 'commentDvr', 'eightPortSwitch', 'powerOn', 'finishedOn'],
+					params: {portNumber: '4'}},
+				ps2: {arr: ['remTcMedia', 'tvNavComp1', 'remAvrGame', 'remFive', 'commentDvr', 'powerOn', 'finishedOn']}
 			};
 
 			return consoleArrs[con];
 		}
 
 		function getConsoleInstructions (insArr, optParams) {
-			var finalInsSet = [];
+			var finalInsSet = [],
+				instruction;
 
-			insArr.forEach(function (item) {
-				var instruction = insList[item];
+			insArr.forEach(function (item, index) {
 				if (item === 'eightPortSwitch') {
-					instruction += optParams.portNumber;
+					var eps = {ins: '', image: ''};
+					eps.ins = insList[item].ins + optParams.portNumber + '"';
+					eps.image = insList[item].image;
+					finalInsSet.push(eps);
+				} else {
+					finalInsSet.push(insList[item]);					
 				}
-				finalInsSet.push(instruction);
+				// instruction = insList[item];
 			});
 
 			return finalInsSet;
