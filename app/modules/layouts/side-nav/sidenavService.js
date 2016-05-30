0cf4a810aa131e38a517054b2a67873ec91a29b0
Joey Gauthier <digitaljedi@outlook.com>
2016-05-30 09:50:17 -0500

    images scaled, menu mostly there

--- app/modules/layouts/side-nav/sidenavService.js
+++ app/modules/layouts/side-nav/sidenavService.js
@@ -19,44 +19,51 @@
 	Menu.$inject = ['$http'];
 
 	function Menu($http) {
-		// Sample code.
-
 		var menu = [
 			{
 				link: '.',
-				name: 'Sega Genesis'
+				name: 'Sega Genesis',
+				image: 'app/assets/images/genesis.png'
 			},
 			{
 				link: '.',
-				name: 'Gamecube'
+				name: 'Gamecube',
+				image: 'app/assets/images/gamecube.png'
 			},
 			{
 				link: '.',
-				name: 'NES'
+				name: 'NES',
+				image: 'app/assets/images/nes.png'
 			},
 			{
 				link: '.',
-				name: 'N64'
+				name: 'N64',
+				image: 'app/assets/images/n64.png'
 			},
 			{
 				link: '.',
-				name: 'PS2'
+				name: 'PS2',
+				image: 'app/assets/images/ps2.png'
 			},
 			{
 				link: '.',
-				name: 'PS3'
+				name: 'PS3',
+				image: 'app/assets/images/ps3.png'
 			},
 			{
 				link: '.',
-				name: 'PC'
+				name: 'PC',
+				image: 'app/assets/images/pc.png'
 			},
 			{
 				link: '.',
-				name: 'Wii U'
+				name: 'Wii U',
+				image: 'app/assets/images/wiiu.png'
 			},
 			{
 				link: '.',
-				name: 'Wii'
+				name: 'Wii',
+				image: 'app/assets/images/wii.png'
 			}
 		];
 
