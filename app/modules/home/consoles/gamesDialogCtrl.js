ad0217a0ec0870d415e16ab0d65477d7909cc4da
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-06-06 10:35:55 -0500

    more dialog logic and templating

--- app/modules/home/consoles/gamesDialogCtrl.js
+++ app/modules/home/consoles/gamesDialogCtrl.js
@@ -18,8 +18,19 @@
 	function GamesDialog (HelpersService, game) {
 		var gd = this,
 			dateFormats = HelpersService.dateFormats();
+
 		gd.gameInfo = game;
 		gd.releaseDate = moment(game.original_release_date).format(dateFormats.abbrMonth);
+
+		gd.gameInfo.genres.forEach(function (item, index) {
+			console.log('item', item);
+			var cleanedName = item.name.replace(/['"]+/g, '');
+			if (index === 0 && item.name) {
+				gd.concattedGenres = cleanedName;
+			} else if (item.name) {
+				gd.concattedGenres += ', ' + cleanedName;
+			}
+		});
 		console.log('from dialog controller', game);
 	}
 })();
