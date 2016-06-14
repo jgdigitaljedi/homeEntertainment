88ccea5ecd97303a067d67ad6bf723a0428722ea
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-06-14 09:12:52 -0500

    cleaned up disc usage and uptime

--- app/modules/home/server/serverCtrl.js
+++ app/modules/home/server/serverCtrl.js
@@ -13,18 +13,41 @@
 		.module('home-control')
 		.controller('ServerCtrl', Server);
 
-	Server.$inject = ['ServerService'];
+	Server.$inject = ['ServerService', '$compile'];
 
-	function Server(ServerService) {
+	function Server(ServerService, $compile) {
 		var sc = this;
 
 		ServerService.getServerInfo().then(function (response) {
 			console.log('response', response);
 			response.forEach(function (item, index) {
-				var nameProp = item.name.split(' ').join('');
+				var nameProp = item.name.split(' ').join(''),
+					temp;
 				sc[nameProp] = item;
-				sc[nameProp].value = item.value.data.replace(/[\u21B5]/g, '\n');
-				sc[nameProp].value = item.value.replace('  ', '\n');
+				sc[nameProp].value.data = item.value.data.replace(/\s+/g, ' ').trim();
+				if (sc[nameProp].name === 'Uptime') {
+					sc[nameProp].value.data = sc[nameProp].value.data.split(' ')[2].replace(',', '');
+				} else if (sc[nameProp].name === 'Disc Space Usage') {
+					temp = sc[nameProp].value.data.replace(/\s\s+/g, ' ');
+					temp = temp.split(' ');
+					var template = '<table><tr>',
+						aLen = temp.length;
+					for (var i = 0; i < aLen; i++) {
+						if (i < 7) {
+							if (i === 6) {
+								template += '<th>' + temp[i] + ' ' + temp[i+1] + '</th></tr><tr>';
+								i++;
+							} else {
+								template += '<th>' + temp[i] + '</th>';
+							}
+						} else {
+							template += '<td>' + temp[i] + '</td>';
+							if ((i) % 7 === 0) template += '</tr><tr>';
+						}
+					}
+					template = $compile(template)(sc);
+					angular.element(document.querySelector('#disc-usage-template')).append(template);
+				}
 				console.log('this thing', sc[nameProp]);
 			});
 		});
