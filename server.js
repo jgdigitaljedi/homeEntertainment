dc1df1940a8f29f4dd4557b1c0388e4bb83fb8a9
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-06-21 08:42:31 -0500

    trying to deal with CORS but pretty sure it is giantbombs server and not mine

--- server.js
+++ server.js
@@ -36,6 +36,12 @@ var Keys = require('./schemas/keys.js');
 // 	console.log('keys', keys);
 // });
 
+app.all('/', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+ });
+
 /********************
 private functions
 ********************/
