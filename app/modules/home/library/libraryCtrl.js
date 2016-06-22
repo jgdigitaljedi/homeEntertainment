45469830b1b1faf71d332078c6025646fdf715cc
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-06-22 12:27:34 -0500

    laying out the library page little by little

--- app/modules/home/library/libraryCtrl.js
+++ app/modules/home/library/libraryCtrl.js
@@ -13,11 +13,97 @@
 		.module('home-control')
 		.controller('LibraryCtrl', Library);
 
-	Library.$inject = ['$scope', '$state'];
+	Library.$inject = ['$scope', '$state', 'LibraryService'];
 
-	function Library ($scope, $state) {
+	function Library ($scope, $state, LibraryService) {
 		var lc = this;
 
-		
+		lc.action = {
+			add: {
+				init: function () {
+					switch(lc.currentTab) {
+						case 'games':
+							console.log('action is add.init with what === ' + lc.currentTab);
+							break;
+						case 'con': 
+							console.log('action is add.init with what === ' + lc.currentTab);
+							break;
+					}					
+				},
+				submit: function (data) {
+					switch(lc.currentTab) {
+						case 'games':
+
+							break;
+						case 'con': 
+
+							break;
+					}
+				}
+			},
+			edit: {
+				init: function () {
+					switch(lc.currentTab) {
+						case 'games':
+							console.log('action is edit.init with what === ' + lc.currentTab);
+							break;
+						case 'con': 
+							console.log('action is edit.init with what === ' + lc.currentTab);
+							break;
+					}					
+				},
+				submit: function (data) {
+					switch(lc.currentTab) {
+						case 'games':
+
+							break;
+						case 'con': 
+
+							break;
+					}
+				}
+			},
+			delete: {
+				init: function () {
+					switch(lc.currentTab) {
+						case 'games':
+							console.log('action is delete.init with what === ' + lc.currentTab);
+							break;
+						case 'con': 
+							console.log('action is delete.init with what === ' + lc.currentTab);
+							break;
+					}					
+				},
+				submit: function (data) {
+					switch(lc.currentTab) {
+						case 'games':
+
+							break;
+						case 'con': 
+
+							break;
+					}
+				}
+			},
+			search: function (data) {
+
+			}
+		};
+
+		lc.changeAction = function (arr) {
+			arr = arr.split(',');
+			lc.action[arr[0]][arr[1]]();
+		};
+
+		function initDefaults () {
+			lc.currentTab = 'search';
+			lc.currentActivity = 'add';
+		}
+
+
+
+		(function () {
+			initDefaults();
+		})();
 	}
 })();
